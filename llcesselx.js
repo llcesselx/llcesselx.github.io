2d7c3f7275982a2342505ec14051952b26a75681
Llcesselx <ltice8@icloud.com>
2022-11-21 13:51:08 -0500

    test2

--- llcesselx.js
+++ llcesselx.js
@@ -1,6 +1,12 @@
-let userName = "";
-
-function getUserName(name) {
-    userName = document.getElementById("u-name");
-    return userName;
+function kittyClick() {
+    let existingImg = document.getElementById("imgChange");
+    let newKittyImg = 'src="https://placekitty.com/250/250"';
+    let newPuppyImg = 'src="https://place-puppy.com/250x250"';
+    if (existingImage.src === "https://placekitty.com/250/250") {
+        existingImage.src = newPuppyImg;
+    }
+    else {
+        existingImg.src = "https://place-puppy.com/250x250";
+    }
+    return existingImg;
 }
