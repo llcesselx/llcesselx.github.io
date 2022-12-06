1fcfd45fc1df035cf8365cb0a3f2ac04fed6ef12
Llcesselx <ltice8@icloud.com>
2022-12-05 22:28:33 -0500

    test2

--- llcesselx.js
+++ llcesselx.js
@@ -1,7 +1,10 @@
 function kittyClick() {
+    console.log("Image clicked...")
+
     let existingImg = document.getElementById("imgChange");
     let newKittyImg = 'src="https://placekitty.com/250/250"';
     let newPuppyImg = 'src="https://place-puppy.com/250x250"';
+
     if (existingImage.src === "https://placekitty.com/250/250") {
         existingImage.src = newPuppyImg;
     }
