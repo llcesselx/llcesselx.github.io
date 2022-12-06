806e041767c3e0ae125ca4ea2b79366cd9b17d31
Llcesselx <ltice8@icloud.com>
2022-12-05 22:32:27 -0500

    test2

--- llcesselx.js
+++ llcesselx.js
@@ -5,11 +5,10 @@ function kittyClick() {
     let newKittyImg = 'src="https://placekitty.com/250/250"';
     let newPuppyImg = 'src="https://place-puppy.com/250x250"';
 
-    if (existingImage.src === "https://placekitty.com/250/250") {
-        existingImage.src = newPuppyImg;
+    if (existingImg.src === "https://placekitty.com/250/250") {
+        existingImg.src = newPuppyImg;
     }
     else {
-        existingImg.src = "https://place-puppy.com/250x250";
+        existingImg.src = newKittyImg;
     }
-    return existingImg;
 }
