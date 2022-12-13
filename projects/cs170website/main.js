e4557bed278478ee77594fedc015b0ceb20cfafa
Llcesselx <ltice8@icloud.com>
2022-12-13 12:51:10 -0500

    github.io

---
+++ projects/cs170website/main.js
@@ -0,0 +1,8 @@
+function setVidVol() {
+    var youtubeVideo = document.getElementById("hate-me");
+    youtubeVideo.setVolume(.5);
+    console.log("playing...");
+}
+function runScr() {
+    console.log("Running Script....");
+}
