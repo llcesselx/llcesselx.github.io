07e36e43e7fd8d0fd612339815a9c7bf63b329c0
llcesselx <108751430+llcesselx@users.noreply.github.com>
2022-12-18 21:26:37 -0500

    created a minesweeper game

---
+++ projects/minesweeper/minesweeper.js
@@ -0,0 +1,153 @@
+function getId() {
+    let sq = event.srcElement.id
+    console.log(sq)
+
+    if (sq === "sq-1" || sq === "sq-3" || sq === "sq-9" || sq === "sq-27" || sq === "sq-32" || sq === "sq-40" || sq === "sq-45" || sq === "sq-52") {
+        console.log("Mine hit!")
+        document.getElementById("sq-1").style.backgroundColor = "red";
+        document.getElementById("sq-3").style.backgroundColor = "red";
+        document.getElementById("sq-9").style.backgroundColor = "red";
+        document.getElementById("sq-27").style.backgroundColor = "red";
+        document.getElementById("sq-32").style.backgroundColor = "red";
+        document.getElementById("sq-40").style.backgroundColor = "red";
+        document.getElementById("sq-45").style.backgroundColor = "red";
+        document.getElementById("sq-52").style.backgroundColor = "red";
+    }
+    if (sq === "sq-5" || sq === "sq-6" || sq === "sq-7" || sq === "sq-15" || sq === "sq-24" || sq === "sq-25") {
+        document.getElementById("sq-5").style.backgroundColor = "green";
+        document.getElementById("sq-6").style.backgroundColor = "green";
+        document.getElementById("sq-7").style.backgroundColor = "green";
+        document.getElementById("sq-15").style.backgroundColor = "green";
+        document.getElementById("sq-24").style.backgroundColor = "green";
+        document.getElementById("sq-25").style.backgroundColor = "green";
+    }
+    if (sq === "sq-47" || sq === "sq-48" || sq === "sq-57" || sq === "sq-58" || sq === "sq-59" || sq === "sq-60") {
+        document.getElementById("sq-47").style.backgroundColor = "green";
+        document.getElementById("sq-48").style.backgroundColor = "green";
+        document.getElementById("sq-57").style.backgroundColor = "green";
+        document.getElementById("sq-58").style.backgroundColor = "green";
+        document.getElementById("sq-59").style.backgroundColor = "green";
+        document.getElementById("sq-60").style.backgroundColor = "green";
+
+    }
+    if (sq === "sq-2") {
+        document.getElementById("sq-2").innerHTML = "2";
+    }
+    if (sq === "sq-4") {
+        document.getElementById("sq-4").innerHTML = "1";
+    }
+    if (sq === "sq-8") {
+        document.getElementById("sq-8").innerHTML = "1";
+    }
+    if (sq === "sq-10") {
+        document.getElementById("sq-10").innerHTML = "1";
+    }
+    if (sq === "sq-11") {
+        document.getElementById("sq-11").innerHTML = "1";
+    }
+    if (sq === "sq-12") {
+        document.getElementById("sq-12").innerHTML = "2";
+    }
+    if (sq === "sq-13") {
+        document.getElementById("sq-13").innerHTML = "1";
+    }
+    if (sq === "sq-14") {
+        document.getElementById("sq-14").innerHTML = "1";
+    }
+    if (sq === "sq-16") {
+        document.getElementById("sq-16").innerHTML = "1";
+    }
+    if (sq === "sq-17") {
+        document.getElementById("sq-17").innerHTML = "1";
+    }
+    if (sq === "sq-18") {
+        document.getElementById("sq-18").innerHTML = "2";
+    }
+    if (sq === "sq-19") {
+        document.getElementById("sq-19").innerHTML = "1";
+    }
+    if (sq === "sq-20") {
+        document.getElementById("sq-20").innerHTML = "1";
+    }
+    if (sq === "sq-21") {
+        document.getElementById("sq-21").innerHTML = "1";
+    }
+    if (sq === "sq-22") {
+        document.getElementById("sq-22").innerHTML = "1";
+    }
+    if (sq === "sq-23") {
+        document.getElementById("sq-23").innerHTML = "1";
+    }
+    if (sq === "sq-26") {
+        document.getElementById("sq-26").innerHTML = "1";
+    }
+    if (sq === "sq-28") {
+        document.getElementById("sq-28").innerHTML = "1";
+    }
+    if (sq === "sq-29") {
+        document.getElementById("sq-29").innerHTML = "1";
+    }
+    if (sq === "sq-30") {
+        document.getElementById("sq-30").innerHTML = "1";
+    }
+    if (sq === "sq-31") {
+        document.getElementById("sq-31").innerHTML = "1";
+    }
+    if (sq === "sq-33") {
+        document.getElementById("sq-33").innerHTML = "1";
+    }
+    if (sq === "sq-34") {
+        document.getElementById("sq-34").innerHTML = "1";
+    }
+    if (sq === "sq-35") {
+        document.getElementById("sq-35").innerHTML = "1";
+    }
+    if (sq === "sq-36") {
+        document.getElementById("sq-36").innerHTML = "2";
+    }
+    if (sq === "sq-37") {
+        document.getElementById("sq-37").innerHTML = "1";
+    }
+    if (sq === "sq-38") {
+        document.getElementById("sq-38").innerHTML = "1";
+    }
+    if (sq === "sq-39") {
+        document.getElementById("sq-39").innerHTML = "1";
+    }
+    if (sq === "sq-41") {
+        document.getElementById("sq-41").innerHTML = "2";
+    }
+    if (sq === "sq-42") {
+        document.getElementById("sq-42").innerHTML = "2";
+    }
+    if (sq === "sq-43") {
+        document.getElementById("sq-43").innerHTML = "2";
+    }
+    if (sq === "sq-44") {
+        document.getElementById("sq-44").innerHTML = "1";
+    }
+    if (sq === "sq-46") {
+        document.getElementById("sq-46").innerHTML = "1";
+    }
+    if (sq === "sq-49") {
+        document.getElementById("sq-49").innerHTML = "1";
+    }
+    if (sq === "sq-50") {
+        document.getElementById("sq-50").innerHTML = "1";
+    }
+    if (sq === "sq-51") {
+        document.getElementById("sq-51").innerHTML = "1";
+    }
+    if (sq === "sq-53") {
+        document.getElementById("sq-53").innerHTML = "1";
+    }
+    if (sq === "sq-54") {
+        document.getElementById("sq-54").innerHTML = "1";
+    }
+    if (sq === "sq-55") {
+        document.getElementById("sq-55").innerHTML = "1";
+    }
+    if (sq === "sq-56") {
+        document.getElementById("sq-56").innerHTML = "1";
+    }
+}
