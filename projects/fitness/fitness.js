559325f277002b396c6c2797958d6d588d35bf90
Llcesselx <ltice8@icloud.com>
2022-12-23 15:47:52 -0500

    Started fitness web app

--- projects/fitness/fitness.js
+++
@@ -1,7 +0,0 @@
-function goBack() {
-    window.location = "https://www.leatice.tech";
-}
-
-function loadGetSwole() {
-    window.location = "https://www.leatice.tech/projects/fitness/getswole/getswole.html";
-}
