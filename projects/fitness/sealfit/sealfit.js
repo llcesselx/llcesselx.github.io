abf150bacd2833f96c7283333a9c265cd2ff6a17
Lea Tice <108751430+llcesselx@users.noreply.github.com>
2026-03-03 17:27:14 -0500

    Modernized sealfit pages

--- projects/fitness/sealfit/sealfit.js
+++ projects/fitness/sealfit/sealfit.js
@@ -1,3 +1,18 @@
+function goBack() {
+    // ../ moves up to 'week1'
+    // ../../ moves up to 'workouts'
+    // ../../../ moves up to 'sealfit' where sealfit.html lives
+    window.location.href = "../../sealfit.html";
+}
+
+// Dynamic Loader for SealFit Workouts
+function loadWorkout(week, day) {
+    // Navigates from sealfit.html into the subfolders
+    // Example: workouts/week1/wk1monday.html
+    window.location.href = `workouts/week${week}/wk${week}${day}.html`;
+}
+
 function gotoHome() {
-    window.location = '../fitness.html';
+    // Moves up one level to the main fitness hub
+    window.location.href = '../fitness.html';
 }
